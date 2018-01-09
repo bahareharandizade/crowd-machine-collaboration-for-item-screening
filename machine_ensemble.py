f548bcc445f08ad3f126f5799fc736b1b47eb3cb
Evgeny Krivosheev <K1r9i9v3osheev>
2018-01-09 22:32:12 +0300

    feature/ machine ensemble

---
+++ machine_ensemble.py
@@ -0,0 +1,94 @@
+import numpy as np
+from helpers.utils import compute_metrics
+
+
+def generate_vote(gt, machine):
+    acc = machine[gt]  # take either acc_positive of acc_negative
+    if np.random.binomial(1, acc):
+        return gt
+    else:
+        return 1 - gt
+
+
+def test_machines_accuracy(filters_num, machines_params):
+    # test machines
+    tests_num = 20  # 20 test items
+    machines_accuracy = [[] for _ in range(filters_num)]
+    for filter_index in range(filters_num):
+        for machine in machines_params[filter_index]:
+            # Indeed,  we need to reject the machine if acc < 0.5
+            while True:
+                correct_votes_num = sum(np.random.binomial(1, machine[0], tests_num // 2)) + \
+                                    sum(np.random.binomial(1, machine[1], tests_num // 2))
+                acc = correct_votes_num / tests_num
+                if acc > 0.5 or acc == 1.:
+                    break
+            machines_accuracy[filter_index].append(acc)
+    return machines_accuracy
+
+
+# fuse votes via weighted majority voting
+# output: probabilities to be negatives for each filter and item
+def weighted_mv(votes_list, filters_num, items_num, machines_accuracy):
+    probs_list = [None]*filters_num*items_num
+    for filter_index in range(filters_num):
+        filter_machines_acc = machines_accuracy[filter_index]
+        for item_index in range(items_num):
+            like_true_val = 1  # assume true value is positive
+            a, b = 1., 1.  # constituents of baysian formula, prior is uniform dist.
+            # a responds for positives, b - for negatives
+            for vote, acc in zip(votes_list[item_index*filters_num + filter_index], filter_machines_acc):
+                if vote == like_true_val:
+                    a *= acc
+                    b *= 1 - acc
+                else:
+                    a *= 1 - acc
+                    b *= acc
+            probs_list[item_index*filters_num + filter_index] = b / (a + b)
+    return probs_list
+
+
+def classify_items(ensembled_vote, lr, filters_num, items_num):
+    prob_pos_list = []
+    items_labels = []
+    pos_thr = lr / (1. + lr)  # threshold to classify as a positive
+    for item_index in range(items_num):
+        prob_all_neg = 1.
+        for filter_index in range(filters_num):
+            prob_all_neg *= ensembled_vote[item_index*filters_num + filter_index]
+        prob_item_pos = 1. - prob_all_neg
+        prob_pos_list.append(prob_item_pos)
+
+        # classify item
+        if prob_item_pos > pos_thr:
+            items_labels.append(0)
+        else:
+            items_labels.append(1)
+    return items_labels, prob_pos_list
+
+
+def machine_ensemble(filters_num, items_num, gt_values, lr):
+    # parameters for machine-based classifiers (accuracy for positives, accuracy for negatives)
+    # positive vote - out of scope
+    # negative vote - in scope
+    machines_params = [[(0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8)],  # machines for criteria 0
+                       [(0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8)],  # machines for criteria 1
+                       [(0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8)],  # machines for criteria 2
+                       [(0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8), (0.9, 0.8)]]  # machines for criteria 3
+
+    votes_list = [[] for _ in range(items_num*filters_num)]
+    for item_index in range(items_num):
+        for filter_index in range(filters_num):
+            gt = gt_values[item_index*filters_num + filter_index]  # can be either 0 or 1
+            for machine in machines_params[filter_index]:
+                vote = generate_vote(gt, machine)
+                votes_list[item_index*filters_num + filter_index].append(vote)
+
+    # estimate machines accuracies
+    machines_accuracy = test_machines_accuracy(filters_num, machines_params)
+    # ensemble votes for each filter and item
+    ensembled_votes = weighted_mv(votes_list, filters_num, items_num, machines_accuracy)
+    items_labels, prob_pos_list = classify_items(ensembled_votes, lr, filters_num, items_num)
+    loss, fp_rate, fn_rate, recall, precision, f_beta = compute_metrics(items_labels, gt_values, lr, filters_num)
+    return loss, fp_rate, fn_rate, recall, precision, f_beta
+
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from helpers.utils import run_quiz_criteria_confm
 from baseline import baseline
 from m_run import m_run
 from sm_run import sm_run
+from machine_ensemble import machine_ensemble
 
 if __name__ == '__main__':
     z = 0.3
-    lr = 5
+    lr = 100
     n_papers = 1000
     papers_page = 10
     criteria_power = [0.14, 0.14, 0.28, 0.42]
@@ -17,44 +18,30 @@ if __name__ == '__main__':
     criteria_num = len(criteria_power)
     fr_p_part = 0.05
     data = []
-    for Nt in range(1, 11, 1):
+    for Nt in [4]:
         for J in [3, 5, 10]:
             print('Nt: {}. J: {}'.format(Nt, J))
-            cost_baseline = (Nt + papers_page * criteria_num) * J / papers_page
-            loss_baseline_list = []
-            fp_b, tp_b, rec_b, pre_b, f_b = [], [], [], [], []
-            loss_mrun_list = []
-            cost_mrun_list = []
-            fp_m, tp_m, rec_m, pre_m, f_m, f_m = [], [], [], [], [], []
+            loss_me_list = []
+            fp_me, tp_me, rec_me, pre_me, f_me, f_me = [], [], [], [], [], []
+
             loss_smrun_list = []
             cost_smrun_list = []
             fp_sm, tp_sm, rec_sm, pre_sm, f_sm, f_sm = [], [], [], [], [], []
-            for _ in range(30):
+            for _ in range(10):
                 # quiz, generation responses
                 acc = run_quiz_criteria_confm(Nt, z, [1.])
                 responses, GT = generate_responses_gt(n_papers, criteria_power, papers_page,
                                                       J, acc, criteria_difficulty)
-                # baseline
-                loss_baseline, fp_rate_b, tp_rate_b, rec_b_, pre_b_, f_beta_b = baseline(responses, criteria_num, n_papers,
-                                                                               papers_page, J, GT, lr)
-                loss_baseline_list.append(loss_baseline)
-                fp_b.append(fp_rate_b)
-                tp_b.append(tp_rate_b)
-                rec_b.append(rec_b_)
-                pre_b.append(pre_b_)
-                f_b.append(f_beta_b)
 
-                # m-run
-                loss_mrun, cost_mrun, fp_rate_m, tp_rate_m, rec_m_, \
-                pre_m_, f_beta_m = m_run(criteria_num, n_papers, papers_page, J, lr, Nt, acc,
-                                         criteria_power, criteria_difficulty, GT, fr_p_part)
-                loss_mrun_list.append(loss_mrun)
-                cost_mrun_list.append(cost_mrun)
-                fp_m.append(fp_rate_m)
-                tp_m.append(tp_rate_m)
-                rec_m.append(rec_m_)
-                pre_m.append(pre_m_)
-                f_m.append(f_beta_m)
+                # machine ensemble
+                loss_me, fp_rate_me, tp_rate_me, \
+                rec_me_, pre_me_, f_beta_me = machine_ensemble(criteria_num, n_papers, GT, lr)
+                loss_me_list.append(loss_me)
+                fp_me.append(fp_rate_me)
+                tp_me.append(tp_rate_me)
+                rec_me.append(rec_me_)
+                pre_me.append(pre_me_)
+                f_me.append(f_beta_me)
 
                 # sm-run
                 loss_smrun, cost_smrun, fp_rate_sm, tp_rate_sm, \
@@ -68,31 +55,27 @@ if __name__ == '__main__':
                 pre_sm.append(pre_sm_)
                 f_sm.append(f_beta_sm)
 
-            print('BASELINE  loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
-                  'recall: {:1.2f}, precision: {:1.2f}, f_b: {}'.\
-                format(np.mean(loss_baseline_list), cost_baseline, np.mean(fp_b), np.mean(tp_b),
-                       np.mean(rec_b), np.mean(pre_b), np.mean(f_b)))
-
-            print('M-RUN     loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
-                  'recall: {:1.2f}, precision: {:1.2f}, f_b: {}'.\
-                format(np.mean(loss_mrun_list), np.mean(cost_mrun_list), np.mean(fp_m), np.mean(tp_m),
-                       np.mean(rec_m), np.mean(pre_m), np.mean(f_m)))
 
             print('SM-RUN    loss: {:1.2f}, price: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
                   'recall: {:1.2f}, precision: {:1.2f}, f_b: {}'.\
                 format(np.mean(loss_smrun_list), np.mean(cost_smrun_list), np.mean(fp_sm), np.mean(tp_sm),
                        np.mean(rec_sm), np.mean(pre_sm), np.mean(f_sm)))
+
+            print('ME-RUN    loss: {:1.2f}, fp_rate: {:1.2f}, tp_rate: {:1.2f}, ' \
+                  'recall: {:1.2f}, precision: {:1.2f}, f_b: {}'. \
+                  format(np.mean(loss_me_list), np.mean(fp_me), np.mean(tp_me),
+                         np.mean(rec_me), np.mean(pre_me), np.mean(f_me)))
             print('---------------------')
 
-            data.append([Nt, J, lr, np.mean(loss_baseline_list), np.std(loss_baseline_list),
-                         np.mean(fp_b), np.mean(tp_b), cost_baseline, 0., 'Baseline',
-                         np.mean(rec_b), np.mean(pre_b), np.mean(f_b)])
-            data.append([Nt, J, lr, np.mean(loss_mrun_list), np.std(loss_mrun_list), np.mean(fp_m),
-                         np.mean(tp_m), np.mean(cost_mrun_list), np.std(cost_mrun_list), 'M-runs',
-                         np.mean(rec_m), np.mean(pre_m), np.mean(f_m)])
+            # data.append([Nt, J, lr, np.mean(loss_baseline_list), np.std(loss_baseline_list),
+            #              np.mean(fp_b), np.mean(tp_b), cost_baseline, 0., 'Baseline',
+            #              np.mean(rec_b), np.mean(pre_b), np.mean(f_b)])
+            # data.append([Nt, J, lr, np.mean(loss_mrun_list), np.std(loss_mrun_list), np.mean(fp_m),
+            #              np.mean(tp_m), np.mean(cost_mrun_list), np.std(cost_mrun_list), 'M-runs',
+            #              np.mean(rec_m), np.mean(pre_m), np.mean(f_m)])
             data.append([Nt, J, lr, np.mean(loss_smrun_list), np.std(loss_smrun_list), np.mean(fp_sm),
                          np.mean(tp_sm), np.mean(cost_smrun_list), np.std(cost_smrun_list), 'SM-runs',
                          np.mean(rec_sm), np.mean(pre_sm), np.mean(f_sm)])
-    pd.DataFrame(data, columns=['Nt', 'J', 'lr', 'loss_mean', 'loss_std', 'FPR', 'TPR',
-                                'price_mean', 'price_std', 'alg', 'recall', 'precision', 'f_beta']). \
-                                to_csv('output/data/cr5_ec20_a_norm.csv', index=False)
+    # pd.DataFrame(data, columns=['Nt', 'J', 'lr', 'loss_mean', 'loss_std', 'FPR', 'TPR',
+    #                             'price_mean', 'price_std', 'alg', 'recall', 'precision', 'f_beta']). \
+    #                             to_csv('output/data/cr5_ec20_a_norm.csv', index=False)
